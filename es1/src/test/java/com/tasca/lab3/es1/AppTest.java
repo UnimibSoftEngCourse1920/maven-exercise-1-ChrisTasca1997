7f7427e06445d470601234852d6d9ddd01d84ba9
christian tasca <c.tasca4@campus.unimib.it>
2020-01-06 18:24:15 +0100

    creazione del progetto

---
+++ es1/src/test/java/com/tasca/lab3/es1/AppTest.java
@@ -0,0 +1,20 @@
+package com.tasca.lab3.es1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
